70b860f42ced41e6bcb7122fef42206491242573
Rock Tsai <r820704@livemail.tw>
2021-03-08 21:47:29 +0800

    新增ApiTest3  用來測試一次讀取多頁的api

--- src/com/place/model/ApiTest3.java
+++ src/com/place/model/ApiTest3.java
@@ -12,7 +12,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class ApiTest {
+public class ApiTest3 {
 	
 	// 取得https://www.travel.taipei/open-api/swagger/ui/index#/Attractions/Attractions_All  的資料
 	public static final String API_URL = "https://www.travel.taipei/open-api/zh-tw/Attractions/All?page=1";
@@ -25,32 +25,7 @@ public class ApiTest {
 //		for(k=1 ; )
 		
 		
-		URL url = new URL(API_URL);
-		HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
-//		HttpRequest request = HttpRequest.newBuilder()
-//		HttpClient client = HttpClient.newBuilder();
-		con.setRequestMethod("GET");
-		con.setRequestProperty("accept", "application/json");
-//		con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-		con.setRequestProperty("User-Agent", "rrrrr");
-		StringBuilder sb = new StringBuilder();
-		if (con.getResponseCode() == 200) {
-			InputStream is = con.getInputStream();
-			InputStreamReader isr = new InputStreamReader(is);
-			BufferedReader br = new BufferedReader(isr);
-
-			String data;
-			while ((data = br.readLine()) != null) {
-				sb.append(data);
-			}
-
-			br.close();
-			isr.close();
-			is.close();
-		} else {
-			System.out.println("No data...");
-		}
 		String jsonstr = sb.toString();
 		JSONObject jsonObj = new JSONObject(jsonstr);
 		JSONArray dataArray = jsonObj.getJSONArray("data");
@@ -126,4 +101,37 @@ public class ApiTest {
 //		System.out.println(dataArray);
 	}
 
+	
+	public StringBuilder getJSON(String targetURL) {
+		
+		URL url = new URL(targetURL);
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+		con.setRequestMethod("GET");
+		con.setRequestProperty("accept", "application/json");
+//		con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+		con.setRequestProperty("User-Agent", "rrrrr");
+		StringBuilder sb = new StringBuilder();
+		if (con.getResponseCode() == 200) {
+			InputStream is = con.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+
+			String data;
+			while ((data = br.readLine()) != null) {
+				sb.append(data);
+			}
+
+			br.close();
+			isr.close();
+			is.close();
+		} else {
+			System.out.println("No data...");
+		}
+		
+		return   ;
+	}
+	
 }
+
+
