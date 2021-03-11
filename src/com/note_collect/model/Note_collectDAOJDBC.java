911fe3e0890ec4ff0fafffd1a71e5599d84e2850
DESKTOP-NCU92DH\Rock <r820704@livemail.tw>
2021-03-11 20:00:49 +0800

    Note_collect修改成複合主鍵

--- src/com/note_collect/model/Note_collectDAOJDBC.java
+++ src/com/note_collect/model/Note_collectDAOJDBC.java
@@ -17,10 +17,11 @@ public class Note_collectDAOJDBC implements Note_collectDAO_interface {
 	String passwd = "123456";
 
 	private static final String INSERT_STMT = "INSERT INTO note_collect (note_id , users_id) VALUES (? , ?)";
-	private static final String GET_ALL_STMT = "SELECT note_collect_id , note_id , users_id , note_collect_time FROM note_collect order by note_collect_id";
-	private static final String GET_ONE_STMT = "SELECT note_collect_id , note_id , users_id , note_collect_time FROM note_collect where note_collect_id = ?";
-	private static final String DELETE = "DELETE FROM note_collect where note_collect_id = ?";
-	private static final String UPDATE = "UPDATE note_collect set note_id=? , users_id=? where note_collect_id = ?";
+	private static final String GET_ALL_STMT = "SELECT  note_id , users_id , note_collect_time FROM note_collect order by users_id";
+	private static final String GET_ONE_STMT = "SELECT  note_id , users_id , note_collect_time FROM note_collect where users_id = ?";
+	private static final String DELETE = "DELETE FROM note_collect where note_id = ? and users_id = ?";
+	// 收藏不會用更新的方式去變更 , 所以更新指令只是練習用的,條件寫死在update方法裡
+	private static final String UPDATE = "UPDATE note_collect set note_id=? , users_id=? where note_id = ? and users_id = ?";
 
 	@Override
 	public void insert(Note_collectVO note_collectVO) {
@@ -67,12 +68,12 @@ public class Note_collectDAOJDBC implements Note_collectDAO_interface {
 		try {
 			Class.forName(driver);
 			con = DriverManager.getConnection(url, userid, passwd);
-			;
 
 			pstmt = con.prepareStatement(UPDATE);
 			pstmt.setObject(1, note_collectVO.getNote_id(), Types.INTEGER);
 			pstmt.setObject(2, note_collectVO.getUsers_id(), Types.INTEGER);
-			pstmt.setObject(3, note_collectVO.getNote_collect_id(), Types.INTEGER);
+			pstmt.setObject(3, 1, Types.INTEGER);
+			pstmt.setObject(4, 1, Types.INTEGER);
 			pstmt.executeUpdate();
 
 		} catch (ClassNotFoundException e) {
@@ -98,7 +99,7 @@ public class Note_collectDAOJDBC implements Note_collectDAO_interface {
 	}
 
 	@Override
-	public void delete(Integer note_collect_id) {
+	public void delete(Integer note_id , Integer users_id) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 
@@ -107,7 +108,8 @@ public class Note_collectDAOJDBC implements Note_collectDAO_interface {
 			con = DriverManager.getConnection(url, userid, passwd);
 			pstmt = con.prepareStatement(DELETE);
 
-			pstmt.setObject(1, note_collect_id, Types.INTEGER);
+			pstmt.setObject(1, note_id, Types.INTEGER);
+			pstmt.setObject(2, users_id, Types.INTEGER);
 			pstmt.executeUpdate();
 
 		} catch (ClassNotFoundException e) {
@@ -150,7 +152,6 @@ public class Note_collectDAOJDBC implements Note_collectDAO_interface {
 			while (rs.next()) {
 				note_collectVO = new Note_collectVO();
 
-				note_collectVO.setNote_collect_id(rs.getInt("note_collect_id"));
 				note_collectVO.setNote_id(rs.getInt("note_id"));
 				note_collectVO.setUsers_id(rs.getInt("users_id"));
 				note_collectVO.setNote_collect_time(rs.getTimestamp("note_collect_time"));
@@ -204,7 +205,6 @@ public class Note_collectDAOJDBC implements Note_collectDAO_interface {
 			while(rs.next()) {
 				note_collectVO = new Note_collectVO();
 				
-				note_collectVO.setNote_collect_id(rs.getInt("note_collect_id"));
 				note_collectVO.setNote_id(rs.getInt("note_id"));
 				note_collectVO.setUsers_id(rs.getInt("users_id"));
 				note_collectVO.setNote_collect_time(rs.getTimestamp("note_collect_time"));
@@ -253,35 +253,32 @@ public class Note_collectDAOJDBC implements Note_collectDAO_interface {
 //		note_collectVO1.setNote_id(2);
 //		note_collectVO1.setUsers_id(1);
 //		dao.insert(note_collectVO1);
-//
+
 //		// 更新
 //		Note_collectVO note_collectVO2 = new Note_collectVO();
 //		note_collectVO2.setNote_id(2);
 //		note_collectVO2.setUsers_id(1);
-//		note_collectVO2.setNote_collect_id(1);
-//		dao.update(note_collectVO2);
 //
+//		dao.update(note_collectVO2);
+
 //		// 刪除
-//		dao.delete(1);
+//		dao.delete(1 , 2);
 
-		// 查詢一筆
-		Note_collectVO note_collectVO3 = dao.findByPrimaryKey(2);
+//		// 查詢一筆
+		Note_collectVO note_collectVO3 = dao.findByPrimaryKey(3);
 
-		System.out.print(note_collectVO3.getNote_collect_id() + ",");
 		System.out.print(note_collectVO3.getNote_id() + ",");
 		System.out.print(note_collectVO3.getUsers_id() + ",");
 		System.out.println(note_collectVO3.getNote_collect_time());
 		System.out.println("---------------------");
-		
-		// 查詢全部
+//		
+//		// 查詢全部
 		List<Note_collectVO> list = dao.getAll();
 		
 		for(Note_collectVO aNote_collect : list) {
-			
-			System.out.print(aNote_collect.getNote_collect_id() + ",");
 			System.out.print(aNote_collect.getNote_id() + ",");
 			System.out.print(aNote_collect.getUsers_id() + ",");
-			System.out.println(aNote_collect.getNote_collect_time());
+			System.out.print(aNote_collect.getNote_collect_time());
 			System.out.println();
 		}
 
--- src/com/note_collect/model/Note_collectDAO_interface.java
+++ src/com/note_collect/model/Note_collectDAO_interface.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface Note_collectDAO_interface {
 	public void insert(Note_collectVO note_collectVO);
 	public void update(Note_collectVO note_collectVO);
-	public void delete(Integer note_collect_id);
-	public Note_collectVO findByPrimaryKey(Integer note_collect_id);
+	public void delete(Integer note_id , Integer users_id);
+	public Note_collectVO findByPrimaryKey(Integer users_id);
 	public List<Note_collectVO> getAll(); 
 }
