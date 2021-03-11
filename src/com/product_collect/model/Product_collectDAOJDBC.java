47cb5abcf70fcd68ca56ef9c711a065e894f6c73
DESKTOP-NCU92DH\Rock <r820704@livemail.tw>
2021-03-11 20:17:30 +0800

    將Product_collect修改為複合主鍵

--- src/com/product_collect/model/Product_collectDAOJDBC.java
+++ src/com/product_collect/model/Product_collectDAOJDBC.java
@@ -17,10 +17,11 @@ public class Product_collectDAOJDBC implements Product_collectDAO_interface {
 	String passwd = "123456";
 
 	private static final String INSERT_STMT = "INSERT INTO product_collect (product_id , users_id) VALUES (? , ?)";
-	private static final String GET_ALL_STMT = "SELECT product_collect_id , product_id , users_id , product_collect_time FROM product_collect order by product_collect_id";
-	private static final String GET_ONE_STMT = "SELECT product_collect_id , product_id , users_id , product_collect_time FROM product_collect where product_collect_id = ?";
-	private static final String DELETE = "DELETE FROM product_collect where product_collect_id=?";
-	private static final String UPDATE = "UPDATE product_collect set product_id=? , users_id=? where product_collect_id=?";
+	private static final String GET_ALL_STMT = "SELECT product_id , users_id , product_collect_time FROM product_collect order by users_id";
+	private static final String GET_ONE_STMT = "SELECT product_id , users_id , product_collect_time FROM product_collect where users_id = ?";
+	private static final String DELETE = "DELETE FROM product_collect where product_id=? and users_id = ?";
+	// 收藏不會用更新的方式去變更 , 所以更新指令只是練習用的,條件寫死在update方法裡
+	private static final String UPDATE = "UPDATE product_collect set product_id=? , users_id=? where product_id=? and users_id = ?";
 
 	@Override
 	public void insert(Product_collectVO product_collectVO) {
@@ -71,8 +72,9 @@ public class Product_collectDAOJDBC implements Product_collectDAO_interface {
 
 			pstmt.setObject(1, product_collectVO.getProduct_id(), Types.INTEGER);
 			pstmt.setObject(2, product_collectVO.getUsers_id(), Types.INTEGER);
-			pstmt.setObject(3, product_collectVO.getProduct_collect_id(), Types.INTEGER);
-
+			pstmt.setObject(3, 1, Types.INTEGER);
+			pstmt.setObject(4, 1, Types.INTEGER);
+			
 			pstmt.executeUpdate();
 		} catch (ClassNotFoundException e) {
 			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
@@ -97,7 +99,7 @@ public class Product_collectDAOJDBC implements Product_collectDAO_interface {
 	}
 
 	@Override
-	public void delete(Integer product_collect_id) {
+	public void delete(Integer product_id , Integer users_id) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 
@@ -106,7 +108,8 @@ public class Product_collectDAOJDBC implements Product_collectDAO_interface {
 			con = DriverManager.getConnection(url, userid, passwd);
 			pstmt = con.prepareStatement(DELETE);
 			
-			pstmt.setObject(1, product_collect_id, Types.INTEGER);
+			pstmt.setObject(1, product_id, Types.INTEGER);
+			pstmt.setObject(2, users_id, Types.INTEGER);
 			pstmt.executeUpdate();
 			
 		} catch (ClassNotFoundException e) {
@@ -132,7 +135,7 @@ public class Product_collectDAOJDBC implements Product_collectDAO_interface {
 	}
 
 	@Override
-	public Product_collectVO findByPrimaryKey(Integer product_collect_id) {
+	public Product_collectVO findByPrimaryKey(Integer users_id) {
 		Product_collectVO product_collectVO = null;
 		Connection con = null;
 		PreparedStatement pstmt = null;
@@ -142,13 +145,12 @@ public class Product_collectDAOJDBC implements Product_collectDAO_interface {
 			Class.forName(driver);
 			con = DriverManager.getConnection(url, userid, passwd);
 			pstmt = con.prepareStatement(GET_ONE_STMT);
-			pstmt.setObject(1, product_collect_id, Types.INTEGER);
+			pstmt.setObject(1, users_id, Types.INTEGER);
 			rs = pstmt.executeQuery();
 			
 			while(rs.next()) {
 				product_collectVO = new Product_collectVO();
 				
-				product_collectVO.setProduct_collect_id(rs.getInt("product_collect_id"));
 				product_collectVO.setProduct_id(rs.getInt("product_id"));
 				product_collectVO.setUsers_id(rs.getInt("users_id"));
 				product_collectVO.setProduct_collect_time(rs.getTimestamp("product_collect_time"));
@@ -204,7 +206,6 @@ public class Product_collectDAOJDBC implements Product_collectDAO_interface {
 			while(rs.next()) {
 				product_collectVO = new Product_collectVO();
 				
-				product_collectVO.setProduct_collect_id(rs.getInt("product_collect_id"));
 				product_collectVO.setProduct_id(rs.getInt("product_id"));
 				product_collectVO.setUsers_id(rs.getInt("users_id"));
 				product_collectVO.setProduct_collect_time(rs.getTimestamp("product_collect_time"));
@@ -248,35 +249,32 @@ public class Product_collectDAOJDBC implements Product_collectDAO_interface {
 
 //		// 新增
 //		Product_collectVO product_collectVO1 = new Product_collectVO();
-//		product_collectVO1.setProduct_id(1);
-//		product_collectVO1.setUsers_id(1);
+//		product_collectVO1.setProduct_id(3);
+//		product_collectVO1.setUsers_id(10);
 //		dao.insert(product_collectVO1);
 //
 //		// 修改
 //		Product_collectVO product_collectVO2 = new Product_collectVO();
 //		product_collectVO2.setProduct_id(1);
 //		product_collectVO2.setUsers_id(2);
-//		product_collectVO2.setProduct_collect_id(1);
 //		dao.update(product_collectVO2);
 //		
 //		// 刪除
-//		dao.delete(1);
+//		dao.delete(1 , 2);
 		
 		// 查詢一筆
 		Product_collectVO product_collectVO3 = dao.findByPrimaryKey(2);
 		
-		System.out.print(product_collectVO3.getProduct_collect_id() + ",");
 		System.out.print(product_collectVO3.getProduct_id() + ",");
 		System.out.print(product_collectVO3.getUsers_id() + ",");
 		System.out.println(product_collectVO3.getProduct_collect_time() );
 		System.out.println("---------------------");
-		
-		// 查詢全部
+//		
+//		// 查詢全部
 		List<Product_collectVO> list = dao.getAll();
 		
 		for(Product_collectVO aProduct_collect : list) {
 			
-			System.out.print(product_collectVO3.getProduct_collect_id() + ",");
 			System.out.print(product_collectVO3.getProduct_id() + ",");
 			System.out.print(product_collectVO3.getUsers_id() + ",");
 			System.out.print(product_collectVO3.getProduct_collect_time() );
--- src/com/product_collect/model/Product_collectDAO_interface.java
+++ src/com/product_collect/model/Product_collectDAO_interface.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface Product_collectDAO_interface {
 	public void insert(Product_collectVO product_collectVO);
 	public void update(Product_collectVO product_collectVO);
-	public void delete(Integer product_collect_id);
-	public Product_collectVO findByPrimaryKey(Integer product_collect_id);
+	public void delete(Integer product_id , Integer users_id);
+	public Product_collectVO findByPrimaryKey(Integer users_id);
 	public List<Product_collectVO> getAll();
 }
