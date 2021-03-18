c76454bdaa0505fe99bc64e1b945c8119f93c595
snk1221 <snk1221@gmail.com>
2021-03-18 10:00:13 +0800

    DAO

--- src/com/card/model/cardDAO_interface.java
+++ src/com/card/model/cardDAO_interface.java
@@ -3,9 +3,9 @@ package com.card.model;
 import java.util.*;
 
 public interface cardDAO_interface {
-	public void insert(cardVO cardVO);
-	public void update(cardVO cardVO);
-	public void delete(Integer card_id);
-	public cardVO findByPK(Integer card_id);
-	public List<cardVO> getAll();
+			public void insert(cardVO cardVO);
+			public void update(cardVO cardVO);
+			public void delete(Integer card_id);
+			public cardVO findByPK(Integer card_id);
+			public List<cardVO> getAll();
 }
