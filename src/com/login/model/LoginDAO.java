9484d7012d720f15c035587f5c4274139bcc2150
Rock Tsai <r820704@livemail.tw>
2021-04-06 14:56:24 +0800

    把/sale-end/sale/login.jsp 改成動態路徑

--- src/com/login/model/LoginDAO.java
+++ src/com/login/model/LoginDAO.java
@@ -18,7 +18,7 @@ public class LoginDAO implements Login_interface{
 	static {
 		try {
 			Context ctx = new InitialContext();
-			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/tea05");
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/David");
 		} catch (NamingException e) {
 			e.printStackTrace();
 		}
