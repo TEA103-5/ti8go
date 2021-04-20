81c4be1be9d6a5131dfe854d23252c89af89e57d
DESKTOP-NCU92DH\Rock <r820704@livemail.tw>
2021-04-20 22:56:41 +0800

    admins 登入時 增加忘記密碼功能, 會產生一組亂數當作新密碼並寄到信箱需javax.mail.jar

--- src/com/admins/controller/AdminsAjaxHandler.java
+++ src/com/admins/controller/AdminsAjaxHandler.java
@@ -16,6 +16,9 @@ import org.json.JSONObject;
 import com.admins.model.AdminsService;
 import com.admins.model.AdminsVO;
 
+import util.MailService_google;
+import util.TokenProcessor;
+
 
 
 public class AdminsAjaxHandler extends HttpServlet {
@@ -106,6 +109,75 @@ public class AdminsAjaxHandler extends HttpServlet {
 			/*************************** 3.查詢完成,準備轉交(Send the Success view) *************/
 			JSONObject resultJSON = new JSONObject(result);
 			out.println(resultJSON);
+			return;
+		}
+		
+		
+		
+		if("forgotPassword".equals(action)) {
+			
+			HashMap result = new HashMap();
+			
+			/*************************** 1.接收請求參數 - 輸入格式的錯誤處理 **********************/
+			String admins_email = req.getParameter("admins_email").trim();
+			
+System.out.println("輸入的信箱=" + admins_email);
+			
+			/*************************** 2.開始查詢資料 *****************************************/
+			AdminsService adminsSvc = new AdminsService();
+			AdminsVO adminsVO = null;
+			List<AdminsVO> list = adminsSvc.getAll();
+			
+			for(AdminsVO oneAdminsVO : list) {
+				if(oneAdminsVO.getAdmins_email().equals(admins_email)) {
+					adminsVO = oneAdminsVO ;
+				}
+			}
+			
+			if(adminsVO == null) {
+				// 即使不符還是回傳已寄出的結果, 但實際上並沒有寄信
+				result.put("result" , "已寄出");
+				JSONObject resultJSON = new JSONObject(result);
+				out.println(resultJSON);
+				return;
+			}
+			
+System.out.println( "符合的信箱=" + adminsVO.getAdmins_email());
+			
+			Integer admins_id = adminsVO.getAdmins_id();
+			admins_email = adminsVO.getAdmins_email();
+			String admins_name = adminsVO.getAdmins_name();
+			
+System.out.println("舊密碼=" + adminsVO.getAdmins_password());
+			TokenProcessor tokenProcessor = TokenProcessor.getInstance();
+			String admins_password = tokenProcessor.makeToken();
+System.out.println("新密碼=" + admins_password);
+			
+			//String admins_password = adminsVO.getAdmins_password();
+			Integer admins_sex = adminsVO.getAdmins_sex();
+			String admins_authority = adminsVO.getAdmins_authority();
+			String admins_position = adminsVO.getAdmins_position();
+						
+			String to = admins_email;
+			String subject = "密碼通知";
+			String ch_name = admins_name;
+			String messageText = "Hello! " + ch_name + " 請謹記此密碼: " + admins_password + "\n" +" (已經啟用)"; 
+			
+			MailService_google mailService = new MailService_google();
+			mailService.sendMail(to, subject, messageText);
+
+try {
+	adminsSvc.updateadmins(admins_id, admins_email, admins_name, admins_password, admins_sex, admins_authority, admins_position);
+} catch (Exception e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+
+			result.put("result" , "已寄出");
+			/*************************** 3.查詢完成,準備轉交(Send the Success view) *************/
+			JSONObject resultJSON = new JSONObject(result);
+			out.println(resultJSON);
+			return;
 		}
 		
 		
---
+++ src/util/MailService_google.java
@@ -0,0 +1,78 @@
+package util;
+
+
+
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class MailService_google {
+	
+	// 設定傳送郵件:至收信人的Email信箱,Email主旨,Email內容
+	public void sendMail(String to, String subject, String messageText) {
+			
+	   try {
+		   // 設定使用SSL連線至 Gmail smtp Server
+		   Properties props = new Properties();
+		   props.put("mail.smtp.host", "smtp.gmail.com");
+		   props.put("mail.smtp.socketFactory.port", "465");
+		   props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+		   props.put("mail.smtp.auth", "true");
+		   props.put("mail.smtp.port", "465");
+
+       // ●設定 gmail 的帳號 & 密碼 (將藉由你的Gmail來傳送Email)
+       // ●須將myGmail的【安全性較低的應用程式存取權】打開
+	     final String myGmail = "limit82704@gmail.com";
+	     final String myGmail_password = "asdf123*";
+		   Session session = Session.getInstance(props, new Authenticator() {
+			   protected PasswordAuthentication getPasswordAuthentication() {
+				   return new PasswordAuthentication(myGmail, myGmail_password);
+			   }
+		   });
+
+		   Message message = new MimeMessage(session);
+		   message.setFrom(new InternetAddress(myGmail));
+		   message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(to));
+		  
+		   //設定信中的主旨  
+		   message.setSubject(subject);
+		   //設定信中的內容 
+		   message.setText(messageText);
+
+		   Transport.send(message);
+		   System.out.println("傳送成功!");
+     }catch (MessagingException e){
+	     System.out.println("傳送失敗!");
+	     e.printStackTrace();
+     }
+   }
+	
+	 public static void main (String args[]){
+
+      String to = "rock820704@gmail.com";
+      
+      String subject = "密碼通知";
+      
+      String ch_name = "David";
+      
+      TokenProcessor tokenProcessor = TokenProcessor.getInstance();
+      String passRandom = tokenProcessor.makeToken();
+      
+//      String passRandom = "111";
+      String messageText = "Hello! " + ch_name + " 請謹記此密碼: " + passRandom + "\n" +" (已經啟用)"; 
+       
+      
+      
+      MailService_google mailService = new MailService_google();
+      mailService.sendMail(to, subject, messageText);
+
+   }
+
+
+}
