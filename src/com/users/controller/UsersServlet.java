55112e1b0ca2c1eecb77d7376a85bf48f11fa18b
snk1221 <snk1221@gmail.com>
2021-04-20 17:42:58 +0800

    增加 USERS 身分證驗證

--- src/com/users/controller/UsersServlet.java
+++ src/com/users/controller/UsersServlet.java
@@ -19,6 +19,8 @@ import com.trip.model.TripVO;
 import com.users.model.UsersService;
 import com.users.model.UsersVO;
 
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
 @MultipartConfig(fileSizeThreshold=1024*1024, maxFileSize=5*1024*1024*1024, maxRequestSize=5*5*1024*1024*1024)
 public class UsersServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -38,7 +40,7 @@ public class UsersServlet extends HttpServlet {
 		
 		String requestUrl = req.getParameter("requestUrl");
 
-		//	System.out.println("action= " + action + " requestUrl= " + requestUrl);
+			System.out.println("action= " + action + " requestUrl= " + requestUrl);
 		
 		
 		
@@ -241,6 +243,39 @@ public class UsersServlet extends HttpServlet {
 				if (users_id_number == null || users_id_number.trim().length() == 0) {
 					errorMsgs.add("身分證請勿空白");
 				}
+				
+				String s = users_id_number;
+				String checkHead = "ABCDEFGHJKLMNPQRSTUVWXYZIO"; // 字母代號對照表
+				if (s.length()==10){
+					char[] c = s.toUpperCase().toCharArray();	// 建立 c 陣列，同時將s字串轉大寫後，轉成字元陣列放入 c 陣列
+					int[] ID = new int [c.length];			// 建立一個運算用的整數陣列，空間為 c 的字元個數
+					// 驗證首位字母是否合法 (該字元是否能在checkHead[]找到), 驗證第一位是否為 1 or 2 (1=男生, 2=女生)
+					if (checkHead.indexOf(c[0]) == -1 || (c[1] != '1' && c[1] != '2'))
+					
+						errorMsgs.add("身分證格式錯誤");
+					
+					else{
+						int sum=0;
+						ID[0] = checkHead.indexOf(c[0])+10;	// 第一個英文字運算
+						sum+=ID[0]/10;				// .. 將商數加總 sum += ID[0]/10 
+						ID[0]%=10;				// .. 取餘數放回 ID[0] 以便之後的運算
+						for (int i=1; i<10; i++)		// 將身分證後9碼轉成整數型態 (ASCII碼-48)
+							ID[i] = (int)c[i]-48;
+						for (int i=0; i<9; i++){		// 代入公式:
+							ID[i]*=(9-i);			// 總和 sum += (ID[0])*9 + ID[1]*8 + ID[2]*7 + ... + ID[9]*1
+							sum+=ID[i];
+						}
+						// 檢查(10-sum%10)是否相等於檢查碼，且 sum%10(餘數)為0時，檢查碼為0 => (10-sum%10)%10
+						if ((10-sum%10)%10 != ID[9])
+							errorMsgs.add("身分證錯誤");
+						
+					}
+				} else {
+					errorMsgs.add("身分證長度錯誤");
+				}
+				        
+				
+				
 				
 				String users_phone = req.getParameter("users_phone").trim();
 				if (users_phone == null || users_phone.trim().length() == 0) {
@@ -473,9 +508,36 @@ public class UsersServlet extends HttpServlet {
 					if (users_id_number == null || users_id_number.trim().length() == 0) {
 						errorMsgs.add("身分證請勿空白");
 					} 
-//					else if(!users_id_number.trim().matches(idReg)) { //以下練習正則(規)表示式(regular-expression)
-//						errorMsgs.add("身分證號碼格式錯誤...");
-//			            }
+					/*** 驗證測試 ***/	
+					String s = users_id_number;
+					String checkHead = "ABCDEFGHJKLMNPQRSTUVWXYZIO"; // 字母代號對照表
+					if (s.length()==10){
+						char[] c = s.toUpperCase().toCharArray();	// 建立 c 陣列，同時將s字串轉大寫後，轉成字元陣列放入 c 陣列
+						int[] ID = new int [c.length];			// 建立一個運算用的整數陣列，空間為 c 的字元個數
+						// 驗證首位字母是否合法 (該字元是否能在checkHead[]找到), 驗證第一位是否為 1 or 2 (1=男生, 2=女生)
+						if (checkHead.indexOf(c[0]) == -1 || (c[1] != '1' && c[1] != '2'))
+						
+							errorMsgs.add("身分證格式錯誤");
+						
+						else{
+							int sum=0;
+							ID[0] = checkHead.indexOf(c[0])+10;	// 第一個英文字運算
+							sum+=ID[0]/10;				// .. 將商數加總 sum += ID[0]/10 
+							ID[0]%=10;				// .. 取餘數放回 ID[0] 以便之後的運算
+							for (int i=1; i<10; i++)		// 將身分證後9碼轉成整數型態 (ASCII碼-48)
+								ID[i] = (int)c[i]-48;
+							for (int i=0; i<9; i++){		// 代入公式:
+								ID[i]*=(9-i);			// 總和 sum += (ID[0])*9 + ID[1]*8 + ID[2]*7 + ... + ID[9]*1
+								sum+=ID[i];
+							}
+							// 檢查(10-sum%10)是否相等於檢查碼，且 sum%10(餘數)為0時，檢查碼為0 => (10-sum%10)%10
+							if ((10-sum%10)%10 != ID[9])
+								errorMsgs.add("身分證錯誤");
+							
+						}
+					} else {
+						errorMsgs.add("身分證長度錯誤");
+					}
 					
 					String users_phone = req.getParameter("users_phone").trim();
 					if (users_phone == null || users_phone.trim().length() == 0) {
